15b231bb94880c36daee5c21ddc71ab0386d5092
JoeSmith <joemsmith111@gmail.com>
2020-02-02 11:30:01 -0500

    adding intermediate Cert Authority and mobile user

--- lab/build_x509.sh
+++ lab/build_x509.sh
@@ -41,7 +41,59 @@ ipsec pki --issue --in gw.csr.pem --lifetime 365 --cacert CA.crt.pem --cakey CA.
 
 ###############################################################################################################
 ###############################################################################################################
+# ICA
+# generate key
+ipsec pki --gen --type rsa --size 4096 --outform pem > ica.key.pem
+
+#generate csr
+ipsec pki --pub --in ica.key.pem --type rsa --outform pem > ica.csr.pem
+
+# sign certificate with root certificate
+ipsec pki --issue --in ica.csr.pem --lifetime 365 --cacert CA.crt.pem --cakey CA.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU=ISS CW2, CN=i_ca.cyber.test" --san 192.168.65.5 --san @192.168.65.5 --flag serverAuth --flag ikeIntermediate --outform pem > ica.crt.pem
+
+###############################################################################################################
+###############################################################################################################
+
+#mobUsr1
+# generate key
+ipsec pki --gen --type rsa --size 4096 --outform pem > mobusr1.key.pem
+
+#generate csr
+ipsec pki --pub --in mobusr1.key.pem --type rsa --outform pem > mobusr1.csr.pem
+
+#sign certificate with Intermediate Certificate authority -- no san due to mobile users having multiple IPs
+ipsec pki --issue --in mobusr1.csr.pem --lifetime 365 --cacert ica.crt.pem --cakey ica.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU =ISS CW2, CN=mobusr1" --flag serverAuth --flag ikeIntermediate --outform pem > mobusr1.crt.pem
+
+###############################################################################################################
+###############################################################################################################
+
+#mobUsr2
+# generate key
+ipsec pki --gen --type rsa --size 4096 --outform pem > mobusr2.key.pem
+
+#generate csr
+ipsec pki --pub --in mobusr2.key.pem --type rsa --outform pem > mobusr2.csr.pem
+
+#sign certificate with Intermediate Certificate authority -- no san due to mobile users having multiple IPs
+ipsec pki --issue --in mobusr2.csr.pem --lifetime 365 --cacert ica.crt.pem --cakey ica.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU =ISS CW2, CN=mobusr2" --flag serverAuth --flag ikeIntermediate --outform pem > mobusr2.crt.pem
+
+###############################################################################################################
+###############################################################################################################
+
+#mobUsr3
+# generate key
+ipsec pki --gen --type rsa --size 4096 --outform pem > mobusr3.key.pem
+
+#generate csr
+ipsec pki --pub --in mobusr3.key.pem --type rsa --outform pem > mobusr3.csr.pem
+
+#sign certificate with Intermediate Certificate authority -- no san due to mobile users having multiple IPs
+ipsec pki --issue --in mobusr3.csr.pem --lifetime 365 --cacert ica.crt.pem --cakey ica.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU =ISS CW2, CN=mobusr3" --flag serverAuth --flag ikeIntermediate --outform pem > mobusr3.crt.pem
+
+###############################################################################################################
+###############################################################################################################
 # move keys/certificates to right place
+
 
 # move CA keys
 mv CA.key.pem ../CA/etc/ipsec.d/private/
@@ -54,6 +106,25 @@ mv gw.crt.pem ../gw.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
 # move www keys
 mv www.key.pem ../www.u1834961-u1824952.cyber.test/etc/ipsec.d/private/
 mv www.crt.pem ../www.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
+
+#move icakeys
+mv ica.key.pem ../i_ca.cyber.test/etc/ipsec.d/private
+mv ica.crt.pem ../i_ca.cyber.test/etc/ipsec.d/cacerts
+
+#move mobusr1 keys
+mv mobusr1.key.pem ../mobusr1/etc/ipsec.d/private
+mv mobusr1.crt.pem ../mobusr1/etc/ipsec.d/cacerts
+
+
+#move mobusr2 keys
+mv mobusr2.key.pem ../mobusr2/etc/ipsec.d/private
+mv mobusr2.crt.pem ../mobusr2/etc/ipsec.d/cacerts
+
+
+#move mobusr1 keys
+mv mobusr3.key.pem ../mobusr3/etc/ipsec.d/private
+mv mobusr3.crt.pem ../mobusr3/etc/ipsec.d/cacerts
+
 
 cd ..
 
