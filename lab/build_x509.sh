6a6c3072b7d746b486e258699b1ea75fed1cb178
Lewis Newman <lewis.newman@warwick.ac.uk>
2020-02-08 23:44:13 +0000

    Added working vpn to the second lab. Certificates and configs are moved about via script so it should be easy to drop it into a new lab if it has the same or similar file structure.

--- lab/build_x509.sh
+++ lab/build_x509.sh
@@ -11,7 +11,9 @@ cd tmp-keys
 ipsec pki --gen --type rsa --size 4096 --outform pem > CA.key.pem
 
 # generate root certificate
-ipsec pki --self --ca --lifetime 3650 --in CA.key.pem --type rsa --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU=ISS CW2, CN=Certificate Authority" --outform pem > CA.crt.pem
+ipsec pki --self --ca --lifetime 3650 --in CA.key.pem --type rsa --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU=ISS CW2, CN=CA" --outform pem > CA.crt.pem
+
+echo "Created Root CA Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
@@ -23,8 +25,10 @@ ipsec pki --gen --type rsa --size 4096 --outform pem > www.key.pem
 # generate csr
 ipsec pki --pub --in www.key.pem --type rsa --outform pem > www.csr.pem
 
-# sign certificate with root certificate
+# sign certificate with root key
 ipsec pki --issue --in www.csr.pem --lifetime 365 --cacert CA.crt.pem --cakey CA.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU=ISS CW2, CN=www.u1834961-u1824952.cyber.test" --san 213.1.133.3 --san @213.1.133.3 --flag serverAuth --flag ikeIntermediate --outform pem > www.crt.pem
+
+echo "Created WWW Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
@@ -36,8 +40,10 @@ ipsec pki --gen --type rsa --size 4096 --outform pem > gw.key.pem
 # generate csr
 ipsec pki --pub --in gw.key.pem --type rsa --outform pem > gw.csr.pem
 
-# sign certificate with root certificate
+# sign certificate with root key
 ipsec pki --issue --in gw.csr.pem --lifetime 365 --cacert CA.crt.pem --cakey CA.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU=ISS CW2, CN=gw.u1834961-u1824952.cyber.test" --san 213.1.133.2 --san @213.1.133.2 --san 192.168.65.1 --san @192.168.65.1 --flag serverAuth --flag ikeIntermediate --outform pem > gw.crt.pem
+
+echo "Created GW Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
@@ -48,8 +54,10 @@ ipsec pki --gen --type rsa --size 4096 --outform pem > ica.key.pem
 #generate csr
 ipsec pki --pub --in ica.key.pem --type rsa --outform pem > ica.csr.pem
 
-# sign certificate with root certificate
-ipsec pki --issue --in ica.csr.pem --lifetime 365 --cacert CA.crt.pem --cakey CA.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU=ISS CW2, CN=i_ca.cyber.test" --san 192.168.65.5 --san @192.168.65.5 --flag serverAuth --flag ikeIntermediate --outform pem > ica.crt.pem
+# sign certificate with root key
+ipsec pki --issue --ca --in ica.csr.pem --lifetime 365 --cacert CA.crt.pem --cakey CA.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU=ISS CW2, CN=i_ca.cyber.test" --san 192.168.65.5 --san @192.168.65.5 --flag serverAuth --flag ikeIntermediate --outform pem > ica.crt.pem
+
+echo "Created Intermediate CA Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
@@ -61,8 +69,10 @@ ipsec pki --gen --type rsa --size 4096 --outform pem > mobusr1.key.pem
 #generate csr
 ipsec pki --pub --in mobusr1.key.pem --type rsa --outform pem > mobusr1.csr.pem
 
-#sign certificate with Intermediate Certificate authority -- no san due to mobile users having multiple IPs
+#sign certificate with Intermediate Certificate authority key -- no san due to mobile users having multiple IPs
 ipsec pki --issue --in mobusr1.csr.pem --lifetime 365 --cacert ica.crt.pem --cakey ica.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU =ISS CW2, CN=mobusr1" --flag serverAuth --flag ikeIntermediate --outform pem > mobusr1.crt.pem
+
+echo "Created mobusr1 Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
@@ -74,8 +84,10 @@ ipsec pki --gen --type rsa --size 4096 --outform pem > mobusr2.key.pem
 #generate csr
 ipsec pki --pub --in mobusr2.key.pem --type rsa --outform pem > mobusr2.csr.pem
 
-#sign certificate with Intermediate Certificate authority -- no san due to mobile users having multiple IPs
+#sign certificate with Intermediate Certificate authority key -- no san due to mobile users having multiple IPs
 ipsec pki --issue --in mobusr2.csr.pem --lifetime 365 --cacert ica.crt.pem --cakey ica.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU =ISS CW2, CN=mobusr2" --flag serverAuth --flag ikeIntermediate --outform pem > mobusr2.crt.pem
+
+echo "Created mobusr2 Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
@@ -87,44 +99,67 @@ ipsec pki --gen --type rsa --size 4096 --outform pem > mobusr3.key.pem
 #generate csr
 ipsec pki --pub --in mobusr3.key.pem --type rsa --outform pem > mobusr3.csr.pem
 
-#sign certificate with Intermediate Certificate authority -- no san due to mobile users having multiple IPs
+#sign certificate with Intermediate Certificate authority key -- no san due to mobile users having multiple IPs
 ipsec pki --issue --in mobusr3.csr.pem --lifetime 365 --cacert ica.crt.pem --cakey ica.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU =ISS CW2, CN=mobusr3" --flag serverAuth --flag ikeIntermediate --outform pem > mobusr3.crt.pem
+
+echo "Created mobusr3 Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
 # move keys/certificates to right place
 
 
-# move CA keys
-mv CA.key.pem ../CA/etc/ipsec.d/private/
-mv CA.crt.pem ../CA/etc/ipsec.d/cacerts/
+# copy CA keys
+cp CA.key.pem ../CA/etc/ipsec.d/private/
+cp CA.crt.pem ../CA/etc/ipsec.d/cacerts/
 
-# move gw keys
-mv gw.key.pem ../gw.u1834961-u1824952.cyber.test/etc/ipsec.d/private/
-mv gw.crt.pem ../gw.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
+# We share the root certificate with all company computers so it can be used to verify all the different certificates/identities for the organisation
 
-# move www keys
-mv www.key.pem ../www.u1834961-u1824952.cyber.test/etc/ipsec.d/private/
-mv www.crt.pem ../www.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
+# copy gw keys
+cp gw.key.pem ../gw.u1834961-u1824952.cyber.test/etc/ipsec.d/private/
+cp gw.crt.pem ../gw.u1834961-u1824952.cyber.test/etc/ipsec.d/certs/
 
-#move icakeys
-mv ica.key.pem ../i_ca.cyber.test/etc/ipsec.d/private
-mv ica.crt.pem ../i_ca.cyber.test/etc/ipsec.d/cacerts
-
-#move mobusr1 keys
-mv mobusr1.key.pem ../mobusr1/etc/ipsec.d/private
-mv mobusr1.crt.pem ../mobusr1/etc/ipsec.d/cacerts
+cp CA.crt.pem ../gw.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
+cp ica.crt.pem ../gw.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
 
 
-#move mobusr2 keys
-mv mobusr2.key.pem ../mobusr2/etc/ipsec.d/private
-mv mobusr2.crt.pem ../mobusr2/etc/ipsec.d/cacerts
+# copy www keys
+cp www.key.pem ../www.u1834961-u1824952.cyber.test/etc/ipsec.d/private/
+cp www.crt.pem ../www.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
+
+cp CA.crt.pem ../www.u1834961-u1824952.cyber.test/etc/ipsec.d/cacerts/
 
 
-#move mobusr1 keys
-mv mobusr3.key.pem ../mobusr3/etc/ipsec.d/private
-mv mobusr3.crt.pem ../mobusr3/etc/ipsec.d/cacerts
 
+# copy icakeys
+cp ica.key.pem ../i_ca.cyber.test/etc/ipsec.d/private/
+cp ica.crt.pem ../i_ca.cyber.test/etc/ipsec.d/cacerts/
+
+cp CA.crt.pem ../i_ca.cyber.test/etc/ipsec.d/cacerts/
+
+
+
+# copy mobusr1 keys
+cp mobusr1.key.pem ../mobusr1/etc/ipsec.d/private/client.key.pem
+cp mobusr1.crt.pem ../mobusr1/etc/ipsec.d/certs/client.crt.pem
+
+cp CA.crt.pem ../mobusr1/etc/ipsec.d/cacerts/
+
+
+
+# copy mobusr2 keys
+cp mobusr2.key.pem ../mobusr2/etc/ipsec.d/private/client.key.pem
+cp mobusr2.crt.pem ../mobusr2/etc/ipsec.d/certs/client.crt.pem
+
+cp CA.crt.pem ../mobusr2/etc/ipsec.d/cacerts/
+
+
+
+# copy mobusr3 keys
+cp mobusr3.key.pem ../mobusr3/etc/ipsec.d/private/client.key.pem
+cp mobusr3.crt.pem ../mobusr3/etc/ipsec.d/certs/client.crt.pem
+
+cp CA.crt.pem ../mobusr3/etc/ipsec.d/cacerts/
 
 cd ..
 
