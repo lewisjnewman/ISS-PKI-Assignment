f661669413b1f69bcb99a7b6a8ea9be1fd54699f
Lewis Newman <lewis.newman@warwick.ac.uk>
2020-02-09 15:31:17 +0000

    Updated scripts so they work

--- Script/build_x509.sh
+++ Script/build_x509.sh
@@ -108,6 +108,29 @@ echo "Created mobusr3 Certificate"
 
 ###############################################################################################################
 ###############################################################################################################
+
+#rogue1
+# generate key
+ipsec pki --gen --type rsa --size 4096 --outform pem > rogue1.key.pem
+
+# generate csr
+ipsec pki --pub --in rogue1.key.pem --type rsa --outform pem > rogue1.csr.pem
+
+#sign certificate with intermediate certificate authority key
+ipsec pki --issue --in rogue1.csr.pem --lifetime 365 --cacert ica.crt.pem --cakey ica.key.pem --dn "C=UK, O=University of Warwick - Cyber Security Centre, OU =ISS CW2, CN=mobusr3" --flag serverAuth --flag ikeIntermediate --outform pem > rogue1.crt.pem
+
+echo "Created rogue1 Certificate"
+
+###############################################################################################################
+###############################################################################################################
+
+#Revoke rogue1 certificate
+ipsec pki --signcrl --cacert ica.crt.pem --cakey ica.key.pem --cert rogue1.crt.pem --outform pem > ica.crl.pem
+
+echo "Revoked rogue1 Certificate"
+
+###############################################################################################################
+###############################################################################################################
 # move keys/certificates to right place
 
 
@@ -155,14 +178,23 @@ cp mobusr2.crt.pem ../lab/mobusr2/etc/ipsec.d/certs/client.crt.pem
 
 cp CA.crt.pem ../lab/mobusr2/etc/ipsec.d/cacerts/
 
-
-
 # copy mobusr3 keys
 cp mobusr3.key.pem ../lab/mobusr3/etc/ipsec.d/private/client.key.pem
 cp mobusr3.crt.pem ../lab/mobusr3/etc/ipsec.d/certs/client.crt.pem
 
 cp CA.crt.pem ../lab/mobusr3/etc/ipsec.d/cacerts/
 
+# copy rogue1 keys
+cp rogue1.key.pem ../lab/rogue1/etc/ipsec.d/private/client.key.pem
+cp rogue1.crt.pem ../lab/rogue1/etc/ipsec.d/certs/client.crt.pem
+
+cp CA.crt.pem ../lab/rogue1/etc/ipsec.d/cacerts/
+
+# copy CRLs
+cp ica.crl.pem ../lab/I_CA/etc/ipsec.d/crls/
+cp ica.crl.pem ../lab/gw.u1834961-u1824952.cyber.test/etc/ipsec.d/crls/
+
+
 cd ..
 
-rm -r tmp-keys
+#rm -r tmp-keys
